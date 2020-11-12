0fc3bea37cfd91cf639de7d91ae081c9ea1a3a99
jpdacosta <jeanpaul.dacosta@gmail.com>
2020-11-12 17:32:50 +1100

    Update

--- DataCollector.py
+++ DataCollector.py
@@ -1,14 +1,17 @@
-class DataCollector:
+class DataCollectorClass:
             
     def collectUserInput(self):
         username =  input("Username: ")
         password = input("Password: ")
         userData = {"Username": username,
                     "Password": password}
+        print(username)
+        print(password)
         return userData
 
 
-from DataCollector import DataCollector
-datCol = DataCollector()
+from DataCollector import DataCollectorClass
+datCol = DataCollectorClass()
 userData = datCol.collectUserInput
-print("Username: " + userData["Username"] + " Password: " + userData["Password"])
+# print(userData["Üsername"])
+# # print("Username: " + userData["Username"] + " Password: " + userData["Password"])
